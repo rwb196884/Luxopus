49341fc5a36ad9b1792e1ac520adabdc54ff28b7
richardb <richardb@truthsoftware.co.uk>
2023-04-03 19:40:48 +0100

    New tariff.

---
+++ Rwb.Luxopus/Jobs/PlanFlux.cs
@@ -0,0 +1,111 @@
+﻿using Microsoft.Extensions.Logging;
+using Rwb.Luxopus.Services;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace Rwb.Luxopus.Jobs
+{
+    /// <summary>
+    /// <para>
+    /// Plan for 'flux' tariff https://octopus.energy/smart/flux/
+    /// </para>
+    /// </summary>
+    public class PlanFlux : Planner
+    {
+        const decimal ExportPrice = 15M;
+
+        /// <summary>
+        /// Rate at which battery discharges to grid. TODO: estimate from historical data.
+        /// Capacity 189 Ah * 55V ~ 10kWh so 1% is 100Wh
+        /// Max charge ~ 4kW => 2.5 hours => 20% per half hour.
+        /// 3kW => 3.5 hours => 15% per half hour.
+        /// TODO: estimate from data.
+        /// </summary>
+        const int BatteryDrainPerHalfHour = 12;
+
+        /// <summary>
+        /// Normal battery minimum allowed level. TODO: estimate from historical data.
+        /// Power to house: day 9am--11pm: 250W, night 11pm--8am: 200W.
+        /// If 1 battery percent is 100Wh then that's 2.5 resp. 2 percent per hour.
+        /// 15 hours over night shoule be about 30% batt.
+        /// </summary>
+        const int BatteryMin = 30;
+
+        private readonly ILuxService _Lux;
+        IEmailService _Email;
+        ISmsService _Sms;
+
+        public PlanFlux(ILogger<LuxMonitor> logger, ILuxService lux, IInfluxQueryService influxQuery, ILuxopusPlanService plan, IEmailService email, ISmsService sms) : base(logger, influxQuery, plan)
+        {
+            _Lux = lux;
+            _Email = email;
+            _Sms = sms;
+        }
+
+        protected override async Task WorkAsync(CancellationToken cancellationToken)
+        {
+            //DateTime t0 = new DateTime(2023, 03, 31, 17, 00, 00);
+            DateTime t0 = new DateTime(2023, 04, 02, 17, 00, 00);
+
+            /*
+             * Flux    20p,  8p 2am to 5am
+             * Daytime 21p, 21p
+             * Peak    46p, 35p 4pm to 7pm
+             * 
+             * Fixed LUX configuration:
+             * Force discharge 16:30 to 17:00 cut off 30
+             * Force charge 02:00 to 05:00 cut off at 30
+             */
+
+            // Get prices and set up plan.
+            DateTime start = t0.StartOfHalfHour();
+            DateTime stop = (new DateTime(t0.Year, t0.Month, t0.Day, 18, 0, 0)).AddDays(1);
+            List<ElectricityPrice> prices = await InfluxQuery.GetPricesAsync(start, stop, "E-1R-FLUX-IMPORT-23-02-14-E", "E-1R-FLUX-EXPORT-23-02-14-E");
+            Plan plan = new Plan(prices);
+
+            // Empty the battery at the peak.
+            // Leave enough to get to 'flux'.
+            // Buy enough in 'flux' to get to daytime generation.
+
+
+
+            PlanService.Save(plan);
+            SendEmail(plan);
+        }
+
+        private void SendEmail(Plan plan)
+        {
+            StringBuilder message = new StringBuilder();
+            foreach (HalfHourPlan p in plan.Plans.OrderBy(z => z.Start))
+            {
+                message.AppendLine(p.ToString());
+            }
+
+            string emailSubjectPrefix = "";
+            if (plan.Plans.Any(z => (z.Action?.DischargeToGrid ?? 101) <= 100))
+            {
+                emailSubjectPrefix += "E";
+            }
+
+            if (plan.Plans.Any(z => (z.Action?.ChargeFromGrid ?? 0) > 0) )
+            {
+                emailSubjectPrefix += "I";
+            }
+
+            string s = message.ToString();
+
+            _Email.SendEmail(emailSubjectPrefix + "Solar strategy " + plan.Plans.First().Start.ToString("dd MMM"), message.ToString());
+
+            if (emailSubjectPrefix.Length > 0)
+            {
+                decimal eveningSellHigh = plan.Plans.Evening().Select(z => z.Sell).Max();
+                decimal overnightBuyMin = plan.Plans.Overnight().Select(z => z.Buy).Min();
+                _Sms.SendSms($"SOLAR! Sell {eveningSellHigh.ToString("00.0")} Buy {overnightBuyMin.ToString("00.0")}");
+            }
+        }
+    }
+}
