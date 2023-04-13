a43ffa489ecb44c5c43e45c1cefb75c8eeb17724
richardb <richardb@truthsoftware.co.uk>
2023-04-13 17:42:13 +0100

    Better discharge prioritisation.

--- Rwb.Luxopus/Jobs/PlanZero.cs
+++ Rwb.Luxopus/Jobs/PlanZero.cs
@@ -3,6 +3,7 @@ using Rwb.Luxopus.Services;
 using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Reactive.Joins;
 using System.Text;
 using System.Threading;
 using System.Threading.Tasks;
@@ -36,7 +37,7 @@ namespace Rwb.Luxopus.Jobs
 
             // Get prices and set up plan.
             DateTime start = t0.StartOfHalfHour();
-            DateTime stop = (new DateTime(t0.Year, t0.Month, t0.Day, 18, 0, 0)).AddDays(1);
+            DateTime stop = (new DateTime(t0.Year, t0.Month, t0.Day, 22, 0, 0)).AddDays(1); // 10pm tomorrow.
 
             List<ElectricityPrice> prices = await InfluxQuery.GetPricesAsync(start, stop, "E-1R-AGILE-FLEX-22-11-25-E", "E-1R-AGILE-OUTGOING-19-05-13-E");
             Plan plan = new Plan(prices);
@@ -54,20 +55,8 @@ namespace Rwb.Luxopus.Jobs
             }
 
             // Export a little at peak time to show willing.
-            HalfHourPlan? maxSell = plan.Plans
-                .Where(z => z.Start < t0.AddHours(22) /* Don't get distracted by tomorrow. */)
-                .OrderByDescending(z => z.Sell)
-                .First();
-            foreach (HalfHourPlan p in plan.Plans.Where(z => z.Start >= maxSell.Start && z.Sell > 15M))
-            {
-                p.Action = new PeriodAction()
-                {
-                    ChargeFromGrid = 0,
-                    //BatteryChargeRate = 0, // Send any generation straight out.
-                    //BatteryGridDischargeRate = 33 + (p.Sell > 15 ? 33 : 0),
-                    DischargeToGrid = 75
-                };
-            }
+            ConfigurePeriod(plan.Plans.Where(z => z.Start.Date == t0.Date));
+            ConfigurePeriod(plan.Plans.Where(z => z.Start.Date == t0.AddDays(1).Date));
 
             // Make room in the battery.
             int battMin = 90 - 15 * (plan?.Plans?.Where(z => z.Buy < 0)?.Count() ?? 0);
@@ -99,6 +88,32 @@ namespace Rwb.Luxopus.Jobs
 
             PlanService.Save(plan);
             SendEmail(plan);
+        }
+        
+        private void ConfigurePeriod(IEnumerable<HalfHourPlan> period)
+        {
+            HalfHourPlan? maxSell = period
+                .OrderByDescending(z => z.Sell)
+                .First();
+            IEnumerable<HalfHourPlan> sellable = period.Where(z => z.Sell > 15M);
+            foreach (HalfHourPlan p in sellable.OrderByDescending(z => z.Sell))
+            {
+                int battMin = 70;
+                if ( p.Start < maxSell.Start)
+                {
+                    battMin += 15 * (sellable.Where(z => z.Start >= p.Start && z.Start < maxSell.Start).Count());
+                }
+                if (battMin < 100)
+                {
+                    p.Action = new PeriodAction()
+                    {
+                        ChargeFromGrid = 0,
+                        //BatteryChargeRate = 0, // Send any generation straight out.
+                        //BatteryGridDischargeRate = 33 + (p.Sell > 15 ? 33 : 0),
+                        DischargeToGrid = battMin
+                    };
+                }
+            }
         }
 
         private void SendEmail(Plan plan)
