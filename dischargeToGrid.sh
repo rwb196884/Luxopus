0953821e20c490f9b6d73e954791d22816e24d6f
Richard Barraclough <rwb@calleva.rwb>
2023-11-15 09:06:15 +0000

    Add script to change discharge to grid.

---
+++ dischargeToGrid.sh
@@ -0,0 +1,30 @@
+#!/bin/sh
+
+planDir="/home/rwb/Luxopus/plans"
+f=$(ls "$planDir" | tail -1)
+
+if [ -z "$1" ]; then
+	echo "Parameter is required."
+	exit 1
+fi
+
+if [ "$1" -eq "$1" ] 2>/dev/null;  then
+	# ok
+	z="z"
+else
+  echo "Parameter must be a number."
+  exit 1
+fi
+
+if [ $1 -lt 0 ]; then
+	echo "Parameter 0 is new charge from grid cut-off. Must be at least 0."
+	exit 1
+elif [ $1 -gt 100 ]; then
+	echo "Parameter 0 is new charge from grid cut-off. Must be at most 100."
+	exit 1
+fi
+ 
+echo "Setting all non-zero DiscahrgeToGrid to $1 in ${planDir}/$f"
+
+cat "${planDir}/$f" | jq ".Plans[].Action.DischargeToGrid |= if . > 0 then $1 else . end" > "${planDir}/${f}.tmp"
+mv "${planDir}/${f}.tmp" "${planDir}/$f"
