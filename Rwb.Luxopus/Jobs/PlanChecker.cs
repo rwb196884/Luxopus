a4f346508b34104b55d123d4c4503f33291882b0
Richard Barraclough <richardb@truthsoftware.co.uk>
2026-06-02 11:48:03 +0100

    Fix scale.

--- Rwb.Luxopus/Jobs/PlanChecker.cs
+++ Rwb.Luxopus/Jobs/PlanChecker.cs
@@ -446,7 +446,7 @@ from(bucket: ""solar"")
                                 why += $" But we are behind by {extraPowerNeeded:0.0}kW and recent generation max is {generationRecentMax / 1000:0.0}kW therefore override to 98%.";
                             }
 
-                            if (bti.ChargeRateNeededHkW < generationRecentMean)
+                            if (bti.ChargeRateNeededHkW < generationRecentMean/ 1000)
                             {
                                 why += $" Need {bti.ChargeNeededHkWH:0.0}kWh in {bti.HoursToCharge:0.0} hours (rate {bti.ChargeRateNeededHkW:0.0}kW, {bti.ChargeRateNeededHPercent}%) but recent generation is {generationRecentMean / 1000:0.0}kW therefore override to 97%.";
                                 battChargeRateWanted = 97;
